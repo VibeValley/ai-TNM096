d0bd6e872ad23821819eb645e3a149ffe3208c0b
Gustav Johansson <xgujoh1999@gmail.com>
2024-04-05 10:48:25 +0200

    last thingy

--- Labb1/main.py
+++ Labb1/main.py
@@ -51,13 +51,12 @@ def allMoves(board, lastVisitedNode):
     
     # 0 - höger | 1 - ner | 2 - vänster | 3 - upp
     allowedMoves = [[0,1],[0,1,2],[1,2],[0,1,3],[0,1,2,3],[1,2,3],[0,3],[0,2,3],[2,3]]
-    indexOfBlank = board.index(0)
-
+    indexOfBlank = board[0].index(0)
     currentMoves = allowedMoves[indexOfBlank]
     
     for n in currentMoves:
         set = True
-        state = copy.copy(board)
+        state = copy.copy(board[0])
         if(n == 0):
             state = moveRight(state)
             h = evaluateH(state)
@@ -70,9 +69,13 @@ def allMoves(board, lastVisitedNode):
         if(n == 3):
             state = moveUp(state)
             h = evaluateH(state)
+        #print(h)
         s = ''.join(str(x) for x in state)
         if s in expandedNodes:
             set = False
+            """ node = expandedNodes[s]
+            if(node > board[1]+1):
+                expandedNodes.update({s:board[1]+1}) """
 
         
         """ for move in lastVisitedNode:
@@ -80,7 +83,7 @@ def allMoves(board, lastVisitedNode):
                 set = False
                 break """
         if(set):
-            movesQ.put((h, state))
+            movesQ.put((h + board[1]+1, [state, board[1]+1,h], board[0]))
 
 def calculateManhattan(board):
     positions = [[1,1], [2,1], [3,1], [1,2], [2,2], [3,2], [1,3], [2,3], [3,3]]
@@ -124,24 +127,43 @@ def main():
     # Ner = +3 index
     print("Welcome to the 8-bit puzzle solver!\n")
     printBoard(board)
-    allMoves(board, expandedNodes)
+    allMoves([board,0], expandedNodes)
     
     numberOfIterations = 0
+    levelInTree = 0; 
     start_time = time.time()
     while(True):
         numberOfIterations = numberOfIterations+1
         node = movesQ.get()
-        if(node[0] == 0):
+        if(node[1][2] == 0):
             break
         allMoves(node[1], expandedNodes)
-        s = ''.join(str(x) for x in node[1])
-        expandedNodes.update({s:node[1]}) 
+        s = ''.join(str(x) for x in node[1][0])
+        expandedNodes.update({s:[node[1][0],node[2]]})
         #lastVisitedNodes.append(node[1])
         #printBoard(node[1])
-        
     #print(expandedNodes)
-    printBoard(node[1])
-    print("Number of iterations: ",numberOfIterations)
+    #printBoard(node[1][0])
+
+    printVector = []
+    firstNode = node[1][0]
+    printVector.append(firstNode)
+    firstNode = node[2]
+    printVector.append(firstNode)
+    for n in range(node[0]):
+        s1 = ''.join(str(x) for x in firstNode)
+        parentNode = expandedNodes[s1][1]
+        printVector.append(parentNode)
+        firstNode = parentNode
+
+    printVector2 = printVector[::-1]
+    for n in printVector2:
+        print("----------------------\n")
+        printBoard(n,)
+        print("\n")
+
+    print("Depth: ", node[0])
+    #print("Number of iterations: ",numberOfIterations)
     end_time = time.time()
     elapsedTime = end_time-start_time
     elapsedTime= round(elapsedTime,4)
